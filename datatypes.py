1719090169c3d55f697c55f1e9c78b1446193907
Umar Murtaza <f2024408061@umt.edu.pk>
2026-02-28 06:14:43 +0500

    added typecasting

--- datatypes.py
+++ datatypes.py
@@ -19,3 +19,16 @@ z = [1, 2, 3, 5]
 x = z    # rebinding x->z x also points to that list now
 x.append(1000)  # any changes in x will also change z
 print(z)
+# typecasting 
+num = "2134.4"  #decimal point str cant be directly cnoverted into int
+print(int(float(num)))
+num1 = 124
+print(str(num1))
+aa = "3.14159"
+print(float(aa)*2)
+ab = "10" 
+ac = 10
+print(str(ac)+ab) # only strings can be concatenated
+ad = 0
+ae = 23
+print(bool(ae) and bool(ad))
