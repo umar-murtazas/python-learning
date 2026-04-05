6b9712c10e79b9e7954ac3bc5e1a44243bd326c4
Umar Murtaza <f2024408061@umt.edu.pk>
2026-04-06 02:01:16 +0500

    methods inside file handling

--- filehandling.py
+++ filehandling.py
@@ -1,4 +1,5 @@
 ## file handling
+'''
 f = open("datatypes.py", "r") #refernced f to this data or it points to it, now f can be used to perform anything on this data
 print("file discription is printed becuase we didnt read() yet : ", f)
 ##<_io.TextIOWrapper name='datatypes.py' mode='r' encoding='cp1252'>
@@ -15,10 +16,33 @@ name = f.read() #pointer will be at the end of the file, nothing new to read
 # name = b.read() # for binary stream
 print("first read() : ", name)
 
-f.seek(10,2)  # resets the pointer, 0->start, 1->current position, 2->end.
+# f.seek(10,2)  # resets the pointer, 0->start, 1->current position, 2->end.   # 2 wouldnt work here its already at the end.
 ## negative bytes doesnt work on text stream
 b.seek(-5,2) # but it works on binary stream, last 5 bytes
 
-name = f.read() # since there is nothing else to read, we get an empty str instead of file data.
-# name = b.read() # for binary stream
+# name = f.read() # since there is nothing else to read, we get an empty str instead of file data.
+name = b.read() # for binary stream
 print("first read() already read everything, hence empty str : ", name)
+
+'''
+f = open("datatypes.py", "r")
+print(f.read(11)) # first 11 characters
+
+r = open("sample.txt", "r")
+print(r.readline()) # reads one line at a time
+print(r.readline()) # reads second line
+print(r.readline()) # reads third line
+print(r.readlines()) # reads all lines into a list
+
+# for line in f:
+#     print(line) # first 11 charcters wouldnt be shown becuase of the above read()
+
+w = open("sample.txt", "w")
+w.write("this is hte new sample data that has been updated inside the file")
+w.close()
+a = open("sample.txt", "a")
+a.write("\nthis is another line that has been added into the file by appending")
+a.close()
+with open("sample.txt", "r") as changed:
+    f = changed.read()
+    print(f)
