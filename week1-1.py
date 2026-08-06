78a0729728ee85c85a5021c94b3ecfcb54b11543
umar-murtazas <f2024408061@umt.edu.pk>
2026-08-07 01:32:04 +0500

    redid the project with improved logic

--- week1-1.py
+++ week1-1.py
@@ -1,50 +1,103 @@
+# # -----------------------methods used----------------------------------- #
+# ## isupper() islower() isdigit() isalnum() isspace()
+# # ----------------------problems----------------------------------------#
+# ## 1) "file" created a file named file instead of var
+# ## 2) count_things() -> functions should return values instead of modify global variables directly
+# ## 3) password_check() -> conditional logic should be readable not complex
+# ## 4) password_check() -> should only count things not print anything.
+# ## 5) second loop should be used only when there are multiple strings in one file.
 
+# file = input("file name : ")
 
+# with open(file, "r") as f:
+#     # f.write("hello This is Me\nwe have bEen working oN this project for years\nthe deaDline is deadlined :)")
+#     content = f.read()
+#     # print(content)
 
+# count = {"uppercase" : 0, "lowercase" : 0, "digit" : 0, "special" : 0}
+# def count_things(content):
+#     for words in content:
+#         for chars in words:
+#             uc = chars.isupper()
+#             if(uc == True):
+#                 count["uppercase"] += 1
+#             lc = chars.islower()
+#             if(lc == True):
+#                 count["lowercase"] += 1
+#             d = chars.isdigit()
+#             if(d == True):
+#                 count["digit"] += 1
+#             sp  = chars.isalnum()
+#             if(sp != True and not(chars.isspace())): 
+#                 count["special"] += 1
+#                 print(chars)
+#     print(count)
 
+# def password_length(content):
+#     return len(content)
 
-file = input("file name : ")
+# # if u want to move in the nested layer under the top condition to not write the entire line again and again use the lower limit
+# def password_check(content):
+#     if(not(password_length(content) <= 7 and (count["lowercase"] <= 0 and count["uppercase"] <= 0 ))): 
+#         if(not(count["digit"] <= 0)):
+#             if(count["special"] > 0):
+#                 print("password is very strong !!!")
+#         else:
+#             print("password is medicore")
+#     else:
+#         print("password is very weak")
 
-with open(file, "r") as f:
-    # f.write("hello This is Me\nwe have bEen working oN this project for years\nthe deaDline is deadlined :)")
-    content = f.read()
-    # print(content)
+# def print_report(c):
+#     count_things(c)
+#     print("totol length is : ", password_length(c))
+#     password_check(c)
+# print_report(content)
 
-count = {"uppercase" : 0, "lowercase" : 0, "digit" : 0, "special" : 0}
-def count_things(content):
-    for words in content:
-        for chars in words:
-            uc = chars.isupper()
-            if(uc == True):
-                count["uppercase"] += 1
-            lc = chars.islower()
-            if(lc == True):
-                count["lowercase"] += 1
-            d = chars.isdigit()
-            if(d == True):
-                count["digit"] += 1
-            sp  = chars.isalnum()
-            if(sp != True and not(chars.isspace())): 
-                count["special"] += 1
-                print(chars)
-    print(count)
+# --------------------------------------retake------------------------------------------------
 
-def password_length(content):
-    return len(content)
+user_file = input("enter the name of the file : ")
+password = ""
+with open(user_file, "r") as file:
+    password = file.read()
+    print(password)
 
-# if u want to move in the nested layer under the top condition to not write the entire line again and again use the lower limit
-def password_check(content):
-    if(not(password_length(content) <= 7 and (count["lowercase"] <= 0 and count["uppercase"] <= 0 ))): 
-        if(not(count["digit"] <= 0)):
-            if(count["special"] > 0):
-                print("password is very strong !!!")
-        else:
-            print("password is medicore")
-    else:
-        print("password is very weak")
+counts = ["uppercase", "lowercase", "digits", "special"]
+def count_things(password):
+    upper = lower = digit = special = 0
+    for string in password:
+        for character in string:
+            if character.isupper():
+                upper += 1
+            elif character.islower():
+                lower += 1
+            elif character.isdigit():
+                digit += 1
+            elif not (character.isalnum() and character.isspace()):
+                special += 1
+            else:
+                print("error")
+    print(string)
+    return upper, lower, digit, special
 
-def print_report(c):
-    count_things(c)
-    print("totol length is : ", password_length(c))
-    password_check(c)
-print_report(content)
+def pass_length(password):
+    return len(password)
+
+def strength_check(counting):
+    if(pass_length(password) < 7):
+        print("length is too short")
+        return
+    if(counting[1] <= 0 and counting[0] <= 0):
+        print("password is weak")
+        return
+    if(counting[2] <= 0 and counting[3] <= 0):
+        print("password is mediocre :)")
+        return
+    print("password is strong !!")
+
+def report(p):
+    counting = count_things(p)
+    total_count = dict(zip(counts, counting))
+    print(total_count)
+    print("password length is : ", pass_length(p))
+    strength_check(counting)
+report(password)
